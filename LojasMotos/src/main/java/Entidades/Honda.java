0c7c41cd47429aa733817b5c66ae2d5dcd7e64e6
Pedro França <89569055+PedroFran2021@users.noreply.github.com>
2024-09-23 18:32:09 -0300

    Estrutura do projeto Loja de Motos
    Estrutura criada

---
+++ LojasMotos/src/main/java/Entidades/Honda.java
@@ -0,0 +1,12 @@
+package Entidades;
+
+public class Honda {
+        public String nome;
+        public  String cilindrada;
+        public String cor;
+        public Honda () {
+            nome = "CBX (Honda)";
+            cilindrada = " 500";
+            cor = "Azul";
+        }
+}
